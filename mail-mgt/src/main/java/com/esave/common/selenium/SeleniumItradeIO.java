4c66a3f80071ad3d5f97e9db92ee96a3ea30e9f6
AshutoshPC <ashutoshsaxena06@gmail.com>
2017-03-23 23:34:11 +0530

    Selenium removed methods and regrex in Mailprocessor

--- mail-mgt/src/main/java/com/esave/common/selenium/SeleniumItradeIO.java
+++ mail-mgt/src/main/java/com/esave/common/selenium/SeleniumItradeIO.java
@@ -1,14 +1,25 @@
 package com.esave.common.selenium;
 
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +29,7 @@ import com.esave.common.Utils;
 import com.esave.entities.OrderDetails;
 
 public class SeleniumItradeIO extends CommonCheneyIO {
-	
+
 	private static final Logger logger = Logger.getLogger(SeleniumItradeIO.class);
 
 	private WebDriverWait wait;
@@ -35,20 +46,52 @@ public class SeleniumItradeIO extends CommonCheneyIO {
 
 		logger.info(userName + " : " + password + " and " + filename);
 		try {
+
 			// Launch setProperty for chrome, Launch, Implicit wait & maximize
 			// Browser
-			driver = Preconditions();
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\ashsaxen\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			// RandomAction.setDownloadFilePath();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			// BrowserAction.ClearBrowserCache(driver);
+			driver.manage().window().maximize();
 
 			// Enter username, pwd and Return successful
-			LoginCheney(driver, userName, password);
+			// LoginCheney(driver, userName, password);
+			driver.get("http://www.procurement.itradenetwork.com/Platform/Membership/Login");
+
+			Thread.sleep(2000);
+
+			// pass login credentials
+			wait = new WebDriverWait(driver, 15);
+			// enter username ##
+			WebElement chb_Username = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[contains(@id,'username')]"))));
+			chb_Username.sendKeys(userName);
+
+			// enter password ##
+			WebElement chb_Password = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[contains(@id,'password')]"))));
+			chb_Password.sendKeys(password);
+
+			driver.findElement(By.xpath("//input[contains(@id,'rememberMe')]")).click();
+
+			// click login
+			WebElement btn_Login = wait.until(ExpectedConditions
+					.elementToBeClickable(driver.findElement(By.xpath("//input[contains(@value,'Login')]"))));
+			btn_Login.click();
+
+			// logger.info("Login Successful");
 
 			// RandomAction.isFramePresent(driver);
 			Thread.sleep(2000);
 
 			// ordering
-			// WebElement lnk_Ordering = wait.until(
-			// ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[contains(.,'Ordering')]"))));
-			// lnk_Ordering.click();
+
+			WebElement lnk_Ordering = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[contains(.,'Ordering')]"))));
+			lnk_Ordering.click();
 
 			// **** Order Products / Entry ***
 			List<WebElement> allElements = wait.until(ExpectedConditions.visibilityOfAllElements(driver
@@ -77,29 +120,241 @@ public class SeleniumItradeIO extends CommonCheneyIO {
 					By.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[1]")))
 					.click().build().perform();
 
-			uploadFile(ss);
+			// uploadFile(ss);
+			try {
+				Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+				Robot robot = new Robot();
+				Thread.sleep(2000);
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				robot.keyPress(KeyEvent.VK_V);
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				robot.keyRelease(KeyEvent.VK_V);
+				Thread.sleep(2000);
+				robot.keyPress(KeyEvent.VK_ENTER);
+				robot.keyRelease(KeyEvent.VK_ENTER);
+			} catch (HeadlessException e) {
+				logger.info("Desktop window upload failed");
+
+			} catch (AWTException e) {
+				logger.info("Desktop window upload failed");
+			}
 
 			logger.info("OrderFile uploaded");
 
 			// Update cart- Checkout1
-			updateCart(driver);
+			// updateCart(driver);
+			try {
+				// Click _UpdateCart
+				// clickUpdatecart();
+				// get Link text
+				WebElement lnk_UpdateCart = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By
+						.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a"))));
+				System.out.println("Link text : " + lnk_UpdateCart.getAttribute("title"));
+
+				Thread.sleep(20000);
+				// Click
+				if (lnk_UpdateCart.getAttribute("title").equalsIgnoreCase("Update Cart")) {
+					WebElement btn_UpdatecCart = wait
+							.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(
+									"//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))));
+					btn_UpdatecCart.click();
+					System.out.println("Clicked on Update Cart");
+				} else {
+					driver.findElement(By
+							.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))
+							.click();
+				}
+
+			} catch (NoSuchElementException ne) {
+				Thread.sleep(5000);
+				// Click _UpdateCart
+				// clickUpdatecart();
+				// get Link text
+				WebElement lnk_UpdateCart = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By
+						.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a"))));
+				System.out.println("Link text : " + lnk_UpdateCart.getAttribute("title"));
+
+				Thread.sleep(20000);
+				// Click
+				if (lnk_UpdateCart.getAttribute("title").equalsIgnoreCase("Update Cart")) {
+					WebElement btn_UpdatecCart = wait
+							.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(
+									"//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))));
+					btn_UpdatecCart.click();
+					System.out.println("Clicked on Update Cart");
+				} else {
+					driver.findElement(By
+							.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))
+							.click();
+				}
+				System.out.println("lnk_UpdateCart not clicked - NoSuchElementException");
+
+			} catch (TimeoutException te) {
+				Thread.sleep(5000);
+				// Click _UpdateCart
+				// clickUpdatecart();
+				// get Link text
+				WebElement lnk_UpdateCart = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By
+						.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a"))));
+				System.out.println("Link text : " + lnk_UpdateCart.getAttribute("title"));
+
+				Thread.sleep(20000);
+				// Click
+				if (lnk_UpdateCart.getAttribute("title").equalsIgnoreCase("Update Cart")) {
+					WebElement btn_UpdatecCart = wait
+							.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(
+									"//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))));
+					btn_UpdatecCart.click();
+					System.out.println("Clicked on Update Cart");
+				} else {
+					driver.findElement(By
+							.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))
+							.click();
+				}
+				System.out.println("lnk_UpdateCart not clicked - TimeoutException");
+
+			} catch (WebDriverException e) {
+				Thread.sleep(5000);
+				// Click _UpdateCart
+				// clickUpdatecart();
+				// get Link text
+				WebElement lnk_UpdateCart = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By
+						.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a"))));
+				System.out.println("Link text : " + lnk_UpdateCart.getAttribute("title"));
+
+				Thread.sleep(20000);
+				// Click
+				if (lnk_UpdateCart.getAttribute("title").equalsIgnoreCase("Update Cart")) {
+					WebElement btn_UpdatecCart = wait
+							.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(
+									"//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))));
+					btn_UpdatecCart.click();
+					System.out.println("Clicked on Update Cart");
+				} else {
+					driver.findElement(By
+							.xpath("//ul[@class='rtbUL']/li[@class='rtbTemplate rtbItem'][2]/following-sibling::li[7]/a/*/*"))
+							.click();
+				}
+				System.out.println("lnk_UpdateCart not clicked - WebDriverException");
+
+			}
 
 			// Pop Up- confirm - Checkout2
-			if (addProductsToCartPopUp(driver) == true) {
-				// Go To Cart
-				goToCart(driver);
+			try {
+
+				// Check the presence of alert
+				Alert alert = driver.switchTo().alert();
+				System.out.println(alert.getText());
+				// if present consume the alert
+				if (alert.getText().equalsIgnoreCase("Add all valid products to your cart?")) {
+					alert.accept();
+					Thread.sleep(3000);
+				} else {
+					System.out.println(alert.getText());
+				}
+			} catch (NoAlertPresentException ex) {
+				// Alert not present
+				ex.printStackTrace();
 			}
+
+			WebElement btn_GoToCart = wait.until(ExpectedConditions
+					.elementToBeClickable(driver.findElement(By.xpath("//div[@class='right-arrow-text'][1]"))));
+			// div[@id='TitleBar']/*/*/div[@id='TitleBarActionNavButtons']/*
+			if (btn_GoToCart.getText().equalsIgnoreCase("Go to Cart")) {
+				btn_GoToCart.click();
+				logger.info("Gotocart");
+			} else {
+				driver.findElement(By.xpath("//div[@class='right-arrow-text'][1]")).click();
+			}
+
+			// if (addProductsToCartPopUp(driver) == true) {
+			// // Go To Cart
+			// //goToCart(driver);
+			//
+			// }
 			// else {
 			//
 			// }
 			// Final- checkout3
-			checkOut(driver);
+			// checkOut(driver);
+			Thread.sleep(2000);
+			WebElement btn_CheckOut = wait.until(ExpectedConditions
+					.elementToBeClickable(driver.findElement(By.xpath("//div[@class='right-arrow-text'][1]"))));
+			if (btn_CheckOut.getText().equalsIgnoreCase("Checkout")) {
+				btn_CheckOut.click();
+				logger.info("Final Checkout");
+			}
 
 			// Validate/ Submit Order
-			validateOrder(driver);
+			// validateOrder(driver);
+			if (driver.getCurrentUrl()
+					.equalsIgnoreCase("procurement.itradenetwork.com/Platform/Orders/Checkout/SelectSubmit")) {
+				// Submit ---#s
+				// submitOrder(driver);
+				// validate/ Submit btn
+				WebElement btn_SubmitOrder = wait.until(ExpectedConditions.elementToBeClickable(
+						driver.findElement(By.xpath("//div[@class='orderInfo category-font']/*/div[7]"))));
+				System.out.println(btn_SubmitOrder.getText());
+				if (btn_SubmitOrder.getText().equalsIgnoreCase("Validate/Submit")) {
+					btn_SubmitOrder.click();
+				}
+
+			} else {
+				Thread.sleep(2000);
+				// logger.info(driver.getCurrentUrl());
+				// Submit ---#
+				// submitOrder(driver);
+				// validate/ Submit btn
+				WebElement btn_SubmitOrder = wait.until(ExpectedConditions.elementToBeClickable(
+						driver.findElement(By.xpath("//div[@class='orderInfo category-font']/*/div[7]"))));
+				System.out.println(btn_SubmitOrder.getText());
+				if (btn_SubmitOrder.getText().equalsIgnoreCase("Validate/Submit")) {
+					btn_SubmitOrder.click();
+				}
+			}
 
 			// Confirm Order Status
-			validateOrderStatus(driver);
+			// validateOrderStatus(driver);
+			if (driver
+					.findElement(By
+							.xpath("//div[@class='ui-dialog ui-widget ui-widget-content ui-corner-all ui-front ui-draggable']"))
+					.isDisplayed()) {
+				try {
+					RandomAction.isIframePresent(driver);
+					driver.switchTo().frame(driver.findElement(By.xpath(
+							"//div[@class='ui-dialog ui-widget ui-widget-content ui-corner-all ui-front ui-draggable']/div[1]/iframe")));
+					logger.info("iFrame captured");
+					WebElement orderText = driver.findElement(
+							By.xpath("//div[@id='orderdetails']/div[1]/div[contains(.,'has been processed']"));
+					logger.info(orderText.getText());
+
+					logger.info("#Success");
+
+					if (orderDetails != null) {
+						try {
+							new Utils().sendNotification(orderDetails.getOrderId(), orderDetails.getPurveyorId(),
+									NotificationEvent.SUCCESS);
+						} catch (IOException e1) {
+							logger.info("Communication failure occured while sending success notification");
+							e1.printStackTrace();
+						}
+					}
+				} catch (Exception e) {
+					if (orderDetails != null) {
+						try {
+							new Utils().sendNotification(orderDetails.getOrderId(), orderDetails.getPurveyorId(),
+									NotificationEvent.SUCCESS);
+						} catch (IOException e1) {
+							logger.info("Communication failure occured while sending success notification");
+							e1.printStackTrace();
+						}
+					}
+					// TODO Auto-generated catch block
+
+					e.printStackTrace();
+				}
+
+			}
 
 			if (orderDetails != null) {
 				try {
@@ -110,7 +365,6 @@ public class SeleniumItradeIO extends CommonCheneyIO {
 					e1.printStackTrace();
 				}
 			}
-
 		} catch (InterruptedException e) {
 
 			logger.info("Failed !!!!" + e.getMessage());
@@ -138,6 +392,7 @@ public class SeleniumItradeIO extends CommonCheneyIO {
 
 		} catch (Exception ex) {
 			logger.info("Failed !!!!" + ex.getMessage());
+			ex.printStackTrace();
 			try {
 				new Utils().sendNotification(orderDetails.getOrderId(), orderDetails.getPurveyorId(),
 						NotificationEvent.FAILURE);
@@ -149,24 +404,15 @@ public class SeleniumItradeIO extends CommonCheneyIO {
 		} finally {
 			// driver.switchTo().parentFrame();
 			// Choose Logout option
-			try {
-				WebElement lnk_Logout = wait.until(ExpectedConditions
-						.elementToBeClickable(driver.findElement(By.xpath("//a[contains(.,'Logout')]"))));
-				lnk_Logout.click();
-				driver.quit();
-			} catch (WebDriverException e) {
-				driver.quit();
-				e.printStackTrace();
-			}
+			driver.quit();
 		}
-
 	}
 
-	// Order push Steps
-
-	// public void OrderPushSteps(WebDriver driver, String orderNo) throws
-	// InterruptedException {
-	//
-	// }
-
 }
+
+// Order push Steps
+
+// public void OrderPushSteps(WebDriver driver, String orderNo) throws
+// InterruptedException {
+//
+// }
