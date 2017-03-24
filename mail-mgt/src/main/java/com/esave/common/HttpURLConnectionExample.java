a1cdaa7fd979f9d76369dc5ebd26f5bbb05f3919
Lalit Arora <Lalit335@gmail.com>
2017-03-24 23:36:42 +0530

    apache http

--- mail-mgt/src/main/java/com/esave/common/HttpURLConnectionExample.java
+++ mail-mgt/src/main/java/com/esave/common/HttpURLConnectionExample.java
@@ -5,6 +5,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class HttpURLConnectionExample {
 
 	private final String USER_AGENT = "Mozilla/5.0";
@@ -49,5 +54,36 @@ public class HttpURLConnectionExample {
 		System.out.println(response.toString());
 
 	}
+	
+	// HTTP GET request
+	private void sendGetHTTPClient() throws Exception {
+
+		String url = "http://www.google.com/search?q=developer";
+
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+
+		// add request header
+		request.addHeader("User-Agent", USER_AGENT);
+
+		HttpResponse response = client.execute(request);
+
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " +
+                       response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+                       new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
+
+	}
+
 
 }
